6dd62deae776536f5b5341812d22f1ae4225d5b0
Nilo Júnior <nsilva@admatic.com.br>
2016-05-27 19:59:53 -0300

    Primeiro commit

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var elixir = require('laravel-elixir');
+//var bowerDir = 'vendor/bower_components/';
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir(function(mix) {
+    mix.sass('app.scss');
+});
