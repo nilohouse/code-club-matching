1c64bd812faccebece69dd058f3187f87bba4557
Nilo Júnior <nsilva@admatic.com.br>
2016-06-11 13:03:27 -0300

    Adicionando HTTP Client ao Composer

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * HttpClient Service Provider
+         */
+        Vinelab\Http\HttpServiceProvider::class
+
     ],
 
     /*
